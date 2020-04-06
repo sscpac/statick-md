7b6784f3433cf645ae41747487195457e24be035
Alexander Xydes <42847381+xydesa@users.noreply.github.com>
2020-04-06 15:49:53 -0700

    0.0.3 (#12)
    * Publishing any tags to test and regular pypi.
    
    * Updating version and CHANGELOG for new release: v0.0.3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     author='NIWC Pacific',
     name='statick-md',
     description='Statick analysis plugins for Markdown files.',
-    version='0.0.2',
+    version='0.0.3',
     packages=['statick_tool',
               'statick_tool.plugins.discovery',
               'statick_tool.plugins.tool'],
