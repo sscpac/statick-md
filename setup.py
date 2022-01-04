d46b84e027c4447cd71f7037d5784feaa5db886b
Thomas Denewiler <tdenewiler@gmail.com>
2022-01-04 08:18:44 -0800

    Update version and CHANGELOG for new release: v0.0.10.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     author="NIWC Pacific",
     name="statick-md",
     description="Statick analysis plugins for Markdown files.",
-    version="0.0.9",
+    version="0.0.10",
     packages=[
         "statick_tool",
         "statick_tool.plugins.discovery",
